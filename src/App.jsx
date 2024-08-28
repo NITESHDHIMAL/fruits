2f5eabfb19022fd17cf0991305b8347fde70ce08
NITESHDHIMAL <niteshdhimal18@gmail.com>
2024-08-28 08:06:45 +0545

    push

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+
+function App() {
+  
+
+  return (
+    <>
+      
+  <Routes>
+    <Route path='/' element={ <Home/> } />
+    <Route path='/shop' element={ <Shop/> } />
+  </Routes>
+   
+   
+    </>
+  )
+}
+
+export default App
